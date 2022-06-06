1e01f4a8012e259b7dc51a18a4e50dafa5dcd809
amir gilboa <amirg76@gmail.com>
2022-06-06 18:14:08 +0300

    update main and media pages

--- src/components/ReviewsCard/ReviewsCard.js
+++ src/components/ReviewsCard/ReviewsCard.js
@@ -2,20 +2,22 @@ import "../ReviewsCard/ReviewsCard.css";
 function ReviewsCard({ data }) {
   return (
     <div className="review-card">
-      <div className="review-card-inner">
-        <div className="review-card-front">
-          <h1>{data.title}</h1>
-          <h3>{data.content}</h3>
-          <div className="review-card-quote">
-            <h1>{data.date}</h1>
-            <h3>{data.username}</h3>
-          </div>
-        </div>
-        {/* <div className="media-card-back">
+      {/* <div className="review-card-inner"> */}
+      <div className="review-card-front">
+        <h1>{data.title}</h1>
+        <h3>{data.content}</h3>
+      </div>
+      <div className="review-card-user">
+        <h3>{data.username}</h3>
+        <h3>{data.date}</h3>
+      </div>
+      {/* </div> */}
+
+      {/* <div className="media-card-back">
           <h2>Trivia</h2>
           <h5>{data.text}</h5>
         </div> */}
-      </div>
+      {/* </div> */}
     </div>
   );
 }
